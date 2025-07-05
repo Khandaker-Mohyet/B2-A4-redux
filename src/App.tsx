d4012d0cf39855f21e23a56a5e0c949e52419473
Khandaker-Mohyet <mohyet008@gmail.com>
2025-07-05 20:42:49 +0600

    add email input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from "./components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 function App() {
   
@@ -9,6 +10,10 @@ function App() {
       <div >
       <Button>Click me</Button>
       <Input type="email" placeholder="Email" />
+      <div className="grid w-full max-w-sm items-center gap-3">
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="Email" />
+    </div>
     </div>
     </>
   )
