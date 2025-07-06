a74251f1343f32f8f22cde09555be33f3c8de458
Khandaker-Mohyet <mohyet008@gmail.com>
2025-07-07 00:42:30 +0600

    create counter slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { Button } from "./components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
 
 function App() {
   
@@ -8,13 +6,7 @@ function App() {
   return (
     <>
       <div >
-      <Button>Click me</Button>
-      <Input type="email" placeholder="Email" />
-      <div className="grid w-full max-w-sm items-center gap-3">
-      <Label htmlFor="email">Email</Label>
-      <Input type="email" id="email" placeholder="Email" />
-      <h1>Nothing to have</h1>
-    </div>
+      
     </div>
     </>
   )
